cdeddf7339b20c9a4c90074cdeab32f7b78ae148
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-08 22:00:44 +0530

    Updates

---
+++ precache-manifest.4f781a2972cb132db528ec17aa6da9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb3fe347dc350e5d8a4ba3e695cbde0",
+    "url": "/personalportfolio/index.html"
+  },
+  {
+    "revision": "e7827588d763f6054e71",
+    "url": "/personalportfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/personalportfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/personalportfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7827588d763f6054e71",
+    "url": "/personalportfolio/static/js/main.6c7eba1f.chunk.js"
+  },
+  {
+    "revision": "a6bede04cf10fd475013",
+    "url": "/personalportfolio/static/js/runtime-main.d67c2a1d.js"
+  }
+]);
